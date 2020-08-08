9f7e7d4d7d6bbda6652f79d2391444706bc2eda4
Nathan Yang <33166425+yhq1119@users.noreply.github.com>
2020-08-08 18:43:37 +1000

    Add files via upload

---
+++ precache-manifest.dbd14f585ce17c15033d82702fa6ebb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e568e00f88a5a0fb9cf9a53334ea36ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d23c875930d873f82db6",
+    "url": "/static/css/main.27acecdf.chunk.css"
+  },
+  {
+    "revision": "f3df0f59904842310b93",
+    "url": "/static/js/2.d35aab28.chunk.js"
+  },
+  {
+    "revision": "d2cf8d37946cd02abf2fe73b5ccb09c4",
+    "url": "/static/js/2.d35aab28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d23c875930d873f82db6",
+    "url": "/static/js/main.387fa6a5.chunk.js"
+  },
+  {
+    "revision": "f5904e665664a826ce1d",
+    "url": "/static/js/runtime-main.6b6c2bfe.js"
+  }
+]);
